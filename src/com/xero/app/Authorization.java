6c7cd09a360c9e5e748d3a46d2608b818e2a9207
SidneyAllen <sid.maestre@gmail.com>
2019-09-09 12:40:04 -0700

    add app.json to try out Heroku deploy button

--- src/com/xero/app/Authorization.java
+++ src/com/xero/app/Authorization.java
@@ -23,9 +23,10 @@ import com.google.api.client.util.store.MemoryDataStoreFactory;
 @WebServlet("/Authorization")
 public class Authorization extends HttpServlet {
     private static final long serialVersionUID = 1L;
-    final String clientId = "--YOUR_CLIENT_ID--";
-    final String clientSecret = "--YOUR_CLIENT_SECRET--";
-    final String redirectURI = "http://localhost:8080/xero-java-oauth2-app/Callback";
+
+    final String clientId = System.getenv("XERO_CLIENT_ID");
+    final String clientSecret = System.getenv("XERO_CLIENT_SECRET");
+    final String redirectURI = System.getenv("XERO_REDIRECT_URI");
 
     final String TOKEN_SERVER_URL = "https://identity.xero.com/connect/token";
     final String AUTHORIZATION_SERVER_URL = "https://login.xero.com/identity/connect/authorize";
